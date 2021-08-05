3ead6e14a6f055bbe6c616cac160eb8b239e6f64
Jose Victor Catalani <josevictorcatalani@gmail.com>
2021-08-05 16:26:09 -0300

    Added Hero and some required settings for grid

--- src/client/pages/_app.tsx
+++ src/client/pages/_app.tsx
@@ -1,15 +1,17 @@
-import type { AppProps } from 'next/app'
-import { ThemeProvider } from 'styled-components';
-import { styledTheme, GlobalStyle } from '../styles/theme';
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import { styledTheme, GlobalStyle } from "../styles/theme";
+import { BaseCSS } from "styled-bootstrap-grid";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <BaseCSS />
       <GlobalStyle />
       <ThemeProvider theme={styledTheme}>
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
-export default MyApp
+export default MyApp;
