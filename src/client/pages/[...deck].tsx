bd61cc04bc3ecf5890123d00cdcfa58f436e066c
Jose Victor Catalani <josevictorcatalani@gmail.com>
2021-08-05 18:30:44 -0300

    Deck routes

--- src/client/pages/[...deck].tsx
+++ src/client/pages/[...deck].tsx
@@ -1,17 +1,37 @@
 import { useRouter } from "next/router";
 import React from "react";
 import App from "../components/App";
+import Jumbotron from "../components/ThemePage/Jumbotron";
 
 const Decks = () => {
   const router = useRouter();
   const { deck }: any = router.query;
   const [theme, deckId, deckFriendlyName] = deck || [];
 
-  console.log({ theme, deckId, deckFriendlyName });
+  const renderTheme = theme && !deckId && !deckFriendlyName;
+  const renderDeck = theme && deckId && deckFriendlyName;
+
+  const renderThemePage = () => {
+    return (
+      <>
+        <h1>Raiz dos temas</h1>
+        <Jumbotron />
+      </>
+    );
+  };
+
+  const renderDeckPage = () => {
+    return (
+      <>
+        <h1>Detalhe do tema</h1>
+        <Jumbotron />
+      </>
+    );
+  };
 
   return (
     <App>
-      <p>Detalhe</p>
+      {renderTheme ? renderThemePage() : renderDeck ? renderDeckPage() : null}
     </App>
   );
 };
