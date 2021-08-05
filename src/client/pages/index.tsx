dac570ccd8839f7c947b8cbba2eae547d6c9ceff
Mateus Gruber <mateus_gruber@hotmail.com.br>
2021-08-05 13:37:26 -0300

    Styled components sample

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -2,26 +2,39 @@ import Head from "next/head";
 import React from "react";
 import App from "../components/App/App";
 import { Container, Row, Col } from "styled-bootstrap-grid";
+import styled from "styled-components";
+
+const Card = styled.div(({theme: { colors }}) => `
+    border: 1px solid ${colors.gray.two};
+    border-radius: 6px;
+    width: 100%;
+    padding: 20px;
+  `,
+);
 
 export default function Landing() {
   return (
     <App>
       <Head>
         <title>Uma forma de estudar interativa</title>
-        <meta
-          name="description"
-          content="Uma forma de estudar interativamente"
-        />
+        <meta name="description" content="Uma forma de estudar interativa" />
       </Head>
+
       <main>
         <Container>
           <Row>
             <Col md={12}>
               <h1>Home</h1>
             </Col>
-            <Col md={4}>Coluna</Col>
-            <Col md={4}>Coluna</Col>
-            <Col md={4}>Coluna</Col>
+            <Col md={4}>
+              <Card>Coluna</Card>
+            </Col>
+            <Col md={4}>
+              <Card>Coluna</Card>
+            </Col>
+            <Col md={4}>
+              <Card>Coluna</Card>
+            </Col>
           </Row>
         </Container>
       </main>
