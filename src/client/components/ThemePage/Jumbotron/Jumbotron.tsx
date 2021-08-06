4c30ca2c46908478477bf43fe9fc0f546babc7cc
Jose Victor Catalani <josevictorcatalani@gmail.com>
2021-08-06 13:17:24 -0300

    header home

--- src/client/components/ThemePage/Jumbotron/Jumbotron.tsx
+++ src/client/components/ThemePage/Jumbotron/Jumbotron.tsx
@@ -1,34 +1,48 @@
 import React, { FC } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { Container, Row, Col } from "styled-bootstrap-grid";
-import { media } from "styled-bootstrap-grid";
 
 const StyledJumbotron = styled.div`
-  background: gray;
-  padding: 80px 0%;
+  padding: 0px 0 0;
 `;
 
-const StyledTitle = styled.h1``;
-const StyledSubTitle = styled.h2``;
-
-const StyledActionItem = styled.div`
-  width: 80px;
-  height: 80px;
-  border-radius: 100%;
-  background: white;
+const StyledTitle = styled.h1`
+  margin-bottom: 25px;
+  font-size: 5.6rem;
 `;
+
+const StyledSubTitle = styled.h2`
+  font-size: 2.4rem;
+  font-weight: 400;
+  letter-spacing: -1px;
+  color: #636769;
+  margin-bottom: 30px;
+`;
+
+const StyledBanner = styled.img(
+  ({ theme: { colors } }) => css`
+    width: 100%;
+    height: 40vh;
+    object-fit: cover;
+    border-bottom: 2px solid ${colors.primary.normal};
+    margin-bottom: 20px;
+  `
+);
 
 const Jumbotron: FC = () => {
   return (
     <StyledJumbotron>
+      <StyledBanner src={"/static/advogado.jpg"} />
       <Container>
         <Row mdJustifyContent="between">
-          <Col mdAuto>
-            <StyledTitle>Title do Jumbotron</StyledTitle>
-            <StyledSubTitle>SubTitle do Jumbotron</StyledSubTitle>
-          </Col>
-          <Col mdAuto>
-            <StyledActionItem />
+          <Col>
+            <StyledTitle>Ciências Sociais</StyledTitle>
+            <StyledSubTitle>
+              Ciências Sociais são uma área que trabalha com a investigação e a
+              pesquisa sobre aspectos relacionados ao comportamento humano. Por
+              isso, para estudar essa disciplina, é importante compreender o
+              funcionamento da sociedade e fenômenos sociais atuais.
+            </StyledSubTitle>
           </Col>
         </Row>
       </Container>
