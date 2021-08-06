4b33c0fa3044e4a16c22ff8acb3eeef44e54bda2
Mateus Gruber <mateus_gruber@hotmail.com.br>
2021-08-06 18:27:13 -0300

    Finishing card

--- src/client/components/DeckPage/Flashcard/Flashcard.tsx
+++ src/client/components/DeckPage/Flashcard/Flashcard.tsx
@@ -87,6 +87,8 @@ const OptionShape = styled.button`
   border-radius: 50%;
   border: none;
   cursor: pointer;
+  background-color: #191A1A;
+  border: 1px solid #313435;
 `;
 
 const FlashcardWrapper = styled.div`
@@ -98,7 +100,8 @@ const DotsPlaceholder = styled.div`
   height: 24px;
   border-radius: 8px;
   margin: 0 47px;
-  background-color: #DBDBDB;
+  color: white;
+  text-align: center;
 `;
 
 const ProgressBar = styled.div<{ percentage: number; backActive: boolean; }>(({ backActive, percentage, theme: { colors } }) => `
@@ -174,6 +177,25 @@ const CardBack = styled(Card)`
   right: 0;
 `;
 
+const AnswerCheck = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+
+  p {
+    display: block;
+    text-align: center;
+  }
+`;
+
+const ButtonsWrapper = styled.div`
+    display: flex;
+
+    button {
+      margin: 0 10px;
+    }
+`;
+
 export interface ICardItem {
   question: string;
   answer: string;
@@ -196,6 +218,7 @@ const Flashcard: FC<IPropsFlashcard> = ({ cardList }) => {
 
   const handleCorrect = () => {
     setCardsDone(cardsDone + 1);
+    setCurrentItem(currentItem === cardList.length - 1 ? 0 : currentItem + 1)
     setShowAnswer(false);
   }
 
@@ -230,31 +253,51 @@ const Flashcard: FC<IPropsFlashcard> = ({ cardList }) => {
           <CardBack>
             <CardContent>
               <CardAnswer>
-                OI
+                {cardList[currentItem].answer}
               </CardAnswer>
               {!!chancesRemaining && (
-                <div>
+                <AnswerCheck>
                   <p>Você acertou a resposta que imaginou?</p><br />
-                  <CustomButton theme="primary" onClick={() => handleCorrect()}>
-                    Sim
-                  </CustomButton>
-                  <CustomButton theme="primaryRed" onClick={() => handleWrong()}>
-                    Não
-                  </CustomButton>
-                </div>
+                  <ButtonsWrapper>
+                    <CustomButton theme="primary" onClick={() => handleCorrect()}>
+                      Sim
+                    </CustomButton>
+                    <CustomButton theme="primaryRed" onClick={() => handleWrong()}>
+                      Não
+                    </CustomButton>
+                  </ButtonsWrapper>
+                </AnswerCheck>
               )}
             </CardContent>
           </CardBack>
         </CardInner>
       </CardWrapper>
       <CardOptions>
-        <OptionShape />
+        <OptionShape>
+          <svg width="16" height="20" viewBox="0 0 16 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12.9995 0.25H2.99953C1.48604 0.25 0.259527 1.48503 0.259527 3L0.25 18.2407C0.249569 18.9311 0.808863 19.4911 1.49922 19.4915C1.66877 19.4916 1.83656 19.4572 1.9924 19.3905L7.90105 16.8582C7.96393 16.8312 8.03512 16.8312 8.09801 16.8582L14.0071 19.3907C14.6417 19.6626 15.3765 19.3687 15.6485 18.7341C15.7151 18.5785 15.7495 18.411 15.7495 18.2417V3C15.7495 1.48579 14.5137 0.25 12.9995 0.25ZM14.2495 17.8626L8.68888 15.4795C8.24867 15.2908 7.75038 15.2908 7.31017 15.4795L1.75024 17.8623L1.75953 3.00047C1.75953 2.31115 2.3168 1.75 2.99953 1.75H12.9995C13.6853 1.75 14.2495 2.31421 14.2495 3V17.8626Z" fill="#CACDCE" />
+          </svg>
+        </OptionShape>
         <OptionsWrapper>
-          <OptionShape onClick={() => setCurrentItem(currentItem === 0 ? cardList.length - 1 : currentItem - 1)} />
-          <DotsPlaceholder />
-          <OptionShape onClick={() => setCurrentItem(currentItem === cardList.length - 1 ? 0 : currentItem + 1)} />
+          <OptionShape onClick={() => setCurrentItem(currentItem === 0 ? cardList.length - 1 : currentItem - 1)}>
+            <svg width="18" height="16" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M7.46967 0.46967C7.76256 0.176777 8.23744 0.176777 8.53033 0.46967C8.82322 0.762563 8.82322 1.23744 8.53033 1.53033L2.809 7.25H17C17.3797 7.25 17.6935 7.53216 17.7432 7.89823L17.75 8C17.75 8.41421 17.4142 8.75 17 8.75H2.811L8.53033 14.4697C8.7966 14.7359 8.8208 15.1526 8.60295 15.4462L8.53033 15.5303C8.23744 15.8232 7.76256 15.8232 7.46967 15.5303L0.46967 8.53033C0.176777 8.23744 0.176777 7.76256 0.46967 7.46967L7.46967 0.46967Z" fill="white" />
+            </svg>
+          </OptionShape>
+          <DotsPlaceholder>
+            {`${currentItem + 1}/${cardList.length}`}
+          </DotsPlaceholder>
+          <OptionShape onClick={() => setCurrentItem(currentItem === cardList.length - 1 ? 0 : currentItem + 1)} >
+            <svg width="18" height="16" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M10.5303 0.46967L17.5303 7.46967C17.8232 7.76257 17.8232 8.23744 17.5303 8.53033L10.5303 15.5303C10.2374 15.8232 9.76256 15.8232 9.46967 15.5303C9.17678 15.2374 9.17678 14.7626 9.46967 14.4697L15.188 8.75H1C0.585786 8.75 0.25 8.41422 0.25 8C0.25 7.58579 0.585786 7.25 1 7.25H15.19L9.46967 1.53033C9.17678 1.23744 9.17678 0.762563 9.46967 0.46967C9.76256 0.176777 10.2374 0.176777 10.5303 0.46967Z" fill="white" />
+            </svg>
+          </OptionShape>
         </OptionsWrapper>
-        <OptionShape />
+        <OptionShape>
+          <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 12.25C1.41421 12.25 1.75 12.5858 1.75 13V16.25H5C5.41421 16.25 5.75 16.5858 5.75 17C5.75 17.4142 5.41421 17.75 5 17.75H1C0.585786 17.75 0.25 17.4142 0.25 17V13C0.25 12.5858 0.585786 12.25 1 12.25ZM17 12.25C17.4142 12.25 17.75 12.5858 17.75 13V17C17.75 17.4142 17.4142 17.75 17 17.75H13C12.5858 17.75 12.25 17.4142 12.25 17C12.25 16.5858 12.5858 16.25 13 16.25H16.25V13C16.25 12.5858 16.5858 12.25 17 12.25ZM17 0.25C17.4142 0.25 17.75 0.585786 17.75 1V5C17.75 5.41421 17.4142 5.75 17 5.75C16.5858 5.75 16.25 5.41421 16.25 5V1.75H13C12.5858 1.75 12.25 1.41421 12.25 1C12.25 0.585786 12.5858 0.25 13 0.25H17ZM5 0.25C5.41421 0.25 5.75 0.585786 5.75 1C5.75 1.41421 5.41421 1.75 5 1.75H1.75V5C1.75 5.41421 1.41421 5.75 1 5.75C0.585786 5.75 0.25 5.41421 0.25 5V1C0.25 0.585786 0.585786 0.25 1 0.25H5Z" fill="#CACDCE" />
+          </svg>
+        </OptionShape>
       </CardOptions>
     </FlashcardWrapper>
   );
